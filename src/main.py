7753e46d93554e418dc727485306df81599a67b4
Funmzy <funmiexalted@yahoo.com>
2024-11-18 15:58:27 +0100

    path_update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+import sys
 import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI
+import uvicorn
 
 from controller.auth_controller import router as auth_router
 from controller.contact_controller import router as contact_router
@@ -10,7 +15,7 @@ load_dotenv()
 
 app = FastAPI(lifespan=db_lifespan)
 
-
+port = int(os.environ.get("PORT", 8000))
 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(contact_router, prefix="/contact", tags=["Contact"])
@@ -20,3 +25,4 @@ app.include_router(contact_router, prefix="/contact", tags=["Contact"])
 @app.get('/')
 def intro():
     return('Welcome to Core CRM')
+
